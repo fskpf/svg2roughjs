579f0202a0999c979f2b3a87dfaa5d1b99c78393
Fabian Schwarzkopf <f.schwarzkopf@yahoo.de>
2020-07-19 15:47:19 +0200

    Improved color gradient approximation

--- index.js
+++ index.js
@@ -703,12 +703,37 @@ export class Svg2Roughjs {
       color.setAlpha(opacity)
       return color.toString()
     } else {
-      // combine the gradient
-      const mixedColor = stops.reduce((acc, stop) => {
-        const color = this.getStopColor(stop)
-        const offset = this.getStopOffset(stop)
-        return tinycolor.mix(acc, color, offset)
-      })
+      // Because roughjs can only deal with solid colors, we try to calculate
+      // the average color of the gradient here.
+      // The idea is to create an array of discrete (average) colors that represents the
+      // gradient under consideration of the stop's offset. Thus, larger offsets
+      // result in more entries of the same mixed color (of the two adjacent color stops).
+      // At the end, this array is averaged again, to create a single solid color.
+      const resolution = 10
+      const discreteColors = []
+
+      let lastColor = null
+      for (let i = 0; i < stops.length; i++) {
+        const currentColor = this.getStopColor(stops[i])
+        const currentOffset = this.getStopOffset(stops[i])
+
+        // combine the adjacent colors
+        const combinedColor = lastColor
+          ? this.averageColor([lastColor, currentColor])
+          : currentColor
+
+        // fill the discrete color array depending on the offset size
+        let entries = Math.max(1, (currentOffset / resolution) | 0)
+        while (entries > 0) {
+          discreteColors.push(combinedColor)
+          entries--
+        }
+
+        lastColor = currentColor
+      }
+
+      // average the discrete colors again for the final result
+      const mixedColor = this.averageColor(discreteColors)
       mixedColor.setAlpha(opacity)
       return mixedColor.toString()
     }
@@ -2239,6 +2264,32 @@ export class Svg2Roughjs {
       }
     }
     return children
+  }
+
+  /**
+   * Calculates the average color of the colors in the given array.
+   * @param {tinycolor[]} colorArray
+   * @returns {tinycolor} The average color
+   */
+  averageColor(colorArray) {
+    const count = colorArray.length
+    let r = 0
+    let g = 0
+    let b = 0
+    let a = 0
+    colorArray.forEach(tinycolor => {
+      const color = tinycolor.toRgb()
+      r += color.r * color.r
+      g += color.g * color.g
+      b += color.b * color.b
+      a += color.a
+    })
+    return tinycolor({
+      r: Math.sqrt(r / count),
+      g: Math.sqrt(g / count),
+      b: Math.sqrt(b / count),
+      a: a / count
+    })
   }
 }
 
