6f82a6b049873f04196b79764ab2e8829ea213c4
Fabian Schwarzkopf <f.schwarzkopf@yahoo.de>
2022-04-15 23:24:37 +0200

    Properly set xlink namespace

--- src/Svg2Roughjs.ts
+++ src/Svg2Roughjs.ts
@@ -273,7 +273,11 @@ export class Svg2Roughjs {
 
     // make sure it has all the proper namespaces
     svgElement.setAttribute('xmlns', 'http://www.w3.org/2000/svg')
-    svgElement.setAttribute('xmlns:xlink', 'http://www.w3.org/1999/xlink')
+    svgElement.setAttributeNS(
+      'http://www.w3.org/2000/xmlns/',
+      'xmlns:xlink',
+      'http://www.w3.org/1999/xlink'
+    )
 
     // clear SVG element
     while (svgElement.firstChild) {
